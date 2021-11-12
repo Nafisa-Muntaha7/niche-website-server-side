aa14798d60717781ce8ecabd5f9ad0937f3c94ef
Nafisa-Muntaha7 <farjanapopi8@gmail.com>
2021-11-12 21:13:25 +0600

    reviewsdb added

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser')
 const app = express();
 const { MongoClient } = require('mongodb');
+const ObjectId = require("mongodb").ObjectId;
 require('dotenv').config();
 const port = process.env.PORT || 7000;
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5mrxq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -20,6 +23,7 @@ async function run() {
         const albumCollection = database.collection('albums');
         const homeAlbumCollection = database.collection('homealbums');
         const upcomingCollection = database.collection('upcomings');
+        const reviewCollection = database.collection('reviews');
 
 
         app.get('/albums', async (req, res) => {
@@ -39,6 +43,17 @@ async function run() {
             const upcomings = await cursor.toArray();
             res.send(upcomings);
         });
+
+        app.post('/review', async (req, res) => {
+            const result = await reviewCollection.insertOne(req.body)
+            res.send(result);
+        });
+
+        app.get('/allReviews', async (req, res) => {
+            const result = await reviewCollection.find({}).toArray();
+            res.send(result);
+            console.log(result);
+        })
 
     }
     finally {
